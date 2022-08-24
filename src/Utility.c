45d3f7aca15588d070a08c95b2050be3b0160bb7
VMO <victor.morales@uv.cl>
2022-08-24 01:20:10 -0500

    man

--- src/Utility.c
+++ src/Utility.c
@@ -1078,7 +1078,7 @@ void DeleteGlobalVar()
   return;
 }
 /*#######################################################################*/
-/*void SetGlobalVar2 (int *nsite, int *times,//2
+void SetGlobalVar2 (int *nsite, int *times,//2
                     double *h,int *nn, double  *maxh,//5
                     double *u,int *tt,  double *maxu,//8
                     int *st,int *biv,int *one,int *two)//12
@@ -1148,7 +1148,7 @@ else{
    }
    // REprintf("1125 utility.c\n");
       return;
-}*/
+}
 
 
 
@@ -1373,77 +1373,6 @@ else{
 }*/
 
 
-void SetGlobalVar2 (int *nsite, int *times,//2
-                    double *h,int *nn, double  *maxh,//5
-                    double *u,int *tt,  double *maxu,//8
-                    int *st,int *biv,int *one,int *two)//12
-{
-
-
-   int i=0;
-
-    int *ncoord=calloc(1,sizeof(int));//number of total spatial coordinates
-  ncoord[0]=&nsite;
-    int *ntime=calloc(1,sizeof(int));//number of times
-  ntime[0]=&times;
-
-    double *maxdist=calloc(1,sizeof   (double));
-  maxdist[0]=&maxh;
-
-    double *maxtime=calloc(1,sizeof   (double));
-  maxtime[0]=*maxu;
-
-    int *npairs=calloc(1,sizeof(int));  // number of pairs involved
-  npairs[0]=nn[0];
-   
-    int *isbiv=calloc(1,sizeof(int));//is a bivariate random field?
-    isbiv[0]=biv[0];
-
-    int *isst=calloc(1,sizeof(int));//is a spatio-temporal random field?
-    isst[0]=st[0];
-
-
-
-   if(!isst[0]&&!isbiv[0]) {  /// spatial case
-       double *lags=calloc(*npairs,sizeof   (double));
-        for (i=0;i<*npairs;i++) lags[i]=h[i];
-    }
-
-else{
-    if(isst[0]) {  /// spatio teemporal case
-        double *lags=calloc(*npairs,sizeof   (double));
-        double *lagt=calloc(*npairs,sizeof   (double));
-        for (i=0;i<*npairs;i++) {lags[i]=h[i];lagt[i]=u[i];}
-    }
- 
-   // REprintf("1108 utility.c\n");
- if(isbiv[0]) {  // spatial bivariate  case
-    //    lags=(double *) Calloc(npairs[0],sizeof   (double));
-    double *lags=calloc(npairs[0],sizeof   (double));
-    
-      //REprintf("1111 utility.c\n");
-        //first=(int *) Calloc(npairs[0],sizeof(int));
-    int *first=calloc(npairs[0],sizeof(int));
-     // REprintf("1113 utility.c *npairs: %d\n",*npairs);
-        //second=(int *) Calloc(npairs[0],sizeof(int));
-    int *second= calloc(npairs[0],sizeof(int));
-     // REprintf("1115 utility.c\n");
-      
-         for (i=0;i<*npairs;i++) {
-            lags[i]=h[i];
-            first[i]=one[i];
-            second[i]=two[i];
-            //REprintf("lags[i] %f  first[i] %d second[i] %d\n",lags[i], first[i],second[i]);
-
-        }
-     // REprintf("1123 utility.c\n");
-
-      }
-    //REprintf("1122 utility.c\n");
-   }
-   // REprintf("1125 utility.c\n");
-      return;
-}
 
 
 /*#######################################################################*/
